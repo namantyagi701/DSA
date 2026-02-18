df9cdfb48bf38d780bced26c377fcd1ccbebda0b
Naman Tyagi <namantyagi98665@gmail.com>
2026-02-18 14:26:13 +0530

    Time: 44 ms (61.73%) | Memory: 54.7 MB (16.63%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    bool f(int i , int j , string& s , string & p,  vector<vector<int>>& dp){
+        if(i == 0 && j == 0) return true;
+        if(j == 0 && i > 0) return false;
+         if(i == 0 && j > 0){
+            for(int k = 1 ; k <= j ; k++){
+                if(p[k-1] != '*') return false;
+            }
+            return dp[i][j] = true;
+         }
+         if(dp[i][j] != -1) return dp[i][j];
+         if(s[i-1] == p[j-1] || p[j-1] == '?') return dp[i][j] = f(i-1 , j-1 , s , p,dp);
+         if(p[j-1] == '*'){
+            return dp[i][j] = f(i-1 , j , s , p,dp) || f(i , j-1 , s , p,dp);
+        }
+        return dp[i][j] = false;
+    }
+public:
+    bool isMatch(string s, string p) {
+        int n = s.size();
+        int m = p.size();
+        vector<vector<int>>dp(n+1 , vector<int>(m+1 , -1));
+        return f(n , m , s , p , dp);
+    }
+};
