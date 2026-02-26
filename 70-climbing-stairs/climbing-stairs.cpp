8547dccb3c5aa01bebcb9915cbfc7f8f2fc3c683
Naman Tyagi <namantyagi98665@gmail.com>
2026-02-26 10:20:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (17.80%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+private:
+    int f(int n, vector<int>&dp){
+        if(n== 0 || n == 1) return n;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = f(n-1 , dp) + f(n-2 , dp);
+    }
+public:
+    int climbStairs(int n) {
+        vector<int>dp(n + 2 , -1); 
+        return f(n + 1 , dp);
+    }
+};
