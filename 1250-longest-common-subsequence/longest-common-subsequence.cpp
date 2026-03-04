6a0f596a5b05617307ca7ee65e55e0e68529e233
Naman Tyagi <namantyagi98665@gmail.com>
2026-03-04 23:43:42 +0530

    Time: 27 ms (58.64%) | Memory: 27.4 MB (53.29%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,22 +1,37 @@
 class Solution {
 private:
-    int f(int i , int j ,string &s1, string &s2 ,  vector<vector<int>> &dp){
-        if(i < 0 || j < 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int match = 0;
-        if(s1[i] == s2[j]){
-            match = 1 + f(i - 1 , j -1 , s1 ,s2 ,dp);
-        }
-        else{
-            match = max(f(i , j-1 , s1 ,s2 ,dp) , f(i-1 , j , s1 ,s2 , dp));
-        }
-        return dp[i][j] = match;
-    }
+    // int f(int i, int j, string& s1, string& s2, vector<vector<int>>& dp) {
+    //     if (i == 0 || j == 0)
+    //         return 0;
+    //     if (dp[i][j] != -1)
+    //         return dp[i][j];
+    //     int match = 0;
+    //     if (s1[i - 1] == s2[j - 1]) {
+    //         match = 1 + f(i - 1, j - 1, s1, s2, dp);
+    //     } else {
+    //         match = max(f(i, j - 1, s1, s2, dp), f(i - 1, j, s1, s2, dp));
+    //     }
+    //     return dp[i][j] = match;
+    // }
+
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.size();
-        int m = text2.size();
-        vector<vector<int>>dp(n , vector<int>(m , -1));
-        return f(n-1 , m-1 , text1 , text2 ,dp);
+    int longestCommonSubsequence(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                // if (i == 0 || j == 0)
+                //     dp[i][j] = 0;
+                int match = 0;
+                if (s1[i - 1] == s2[j - 1]) {
+                    match = 1 + dp[i-1][j-1];
+                } else {
+                    match = max(dp[i][j-1], dp[i-1][j]);
+                }
+                dp[i][j] = match;
+            }
+        }
+        return dp[n][m];
     }
 };
