3774685bd0632d74cac7f2180db279f956061f54
Naman Tyagi <namantyagi98665@gmail.com>
2026-03-05 17:20:31 +0530

    Time: 11 ms (66.69%) | Memory: 16.9 MB (22.96%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,22 +1,18 @@
 class Solution {
 private:
-    int f(int i , int j , string word1 , string word2, vector<vector<int>> &dp){
-        if(i < 0 || j < 0) return 0;
-        
+    int f(int i , int j , string &s1 , string &s2 ,vector<vector<int>> &dp){
+        if(i == 0 || j == 0) return 0;
         if(dp[i][j] != -1) return dp[i][j];
-        if(word1[i] == word2[j]) return 1 + f(i-1 , j-1 , word1, word2 , dp);
-        else{
-            return dp[i][j] = max(f(i-1 , j , word1 , word2 , dp) , f(i , j-1 , word1 , word2 , dp));
-        }
+        if(s1[i-1] == s2[j-1]) return dp[i][j] = 1 + f(i-1 , j-1 , s1 ,s2 , dp);
+        return dp[i][j] = max(f(i-1 , j , s1 ,s2 , dp) , f(i , j-1 , s1 ,s2 , dp));
     }
 public:
     int minDistance(string word1, string word2) {
-       int i = word1.size();
-       int j = word2.size();
-       int total = i+j;
-
-       vector<vector<int>> dp(i , vector<int>(j , -1));
-       int ans = f(i-1 , j-1 , word1 , word2,dp);
-       return total - 2*ans;       
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n+1 , vector<int>(m+1 , -1));
+        int k = f(n , m , word1 , word2 , dp);
+        int ans = (n+m) - (2 *k);
+        return ans;
     }
 };
