088b95d788ac099041170286fa88d52b4e4fb4c6
Naman Tyagi <namantyagi98665@gmail.com>
2026-03-07 23:45:02 +0530

    Time: 51 ms (65.42%) | Memory: 79.5 MB (68.73%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -23,24 +23,26 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(
-            n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+        // vector<vector<vector<int>>> dp(
+        //     n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+        vector<vector<int>> curr(2, vector<int>(3, 0)) , next(2, vector<int>(3, 0));
         for (int i = n - 1 ; i >= 0; i--) {
             for (int buy = 0; buy < 2; buy++) {
                 for (int cap = 1; cap < 3; cap++) {
     
                     if (buy) {
-                        dp[i][buy][cap] = max(
-                                   -prices[i] + dp[i + 1][0][cap],
-                                   0 + dp[i+1][1][cap]);
+                        curr[buy][cap] = max(
+                                   -prices[i] + next[0][cap],
+                                   0 + next[1][cap]);
                     } else {
-                        dp[i][buy][cap] = max(
-                                   prices[i] + dp[i+1][1][cap-1],
-                                   0 + dp[i + 1][0][cap]);
+                        curr[buy][cap] = max(
+                                   prices[i] + next[1][cap-1],
+                                   0 + next[0][cap]);
                     }
                 }
             }
+            next = curr;
         }
-        return dp[0][1][2];
+        return next[1][2];
     }
 };
