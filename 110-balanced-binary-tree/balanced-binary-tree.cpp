c31877e55ac760a0fe4d8ddc073f59f2aad23f9a
Naman Tyagi <namantyagi98665@gmail.com>
2026-04-16 14:13:02 +0530

    Time: 0 ms (100.00%) | Memory: 23.2 MB (20.18%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -6,27 +6,23 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int f(TreeNode* node) {
-        if (node == NULL)
-            return 0;
-
-        int countL = 1 + f(node->left);
-        int countR = 1 + f(node->right);
-
-        return max(countL, countR);
-    }
     bool isBalanced(TreeNode* root) {
-        if(root == nullptr) return true;
-        int lh = f(root -> left);
-        int rh = f(root -> right);
+        return f(root) != -1;
+    }
+    int f(TreeNode * node){
+        if(node == NULL) return 0;
+        int lh = f(node -> left);
+        if(lh == -1) return -1;
 
-        if(abs(lh - rh) <= 1 && isBalanced(root -> left) && isBalanced(root -> right)) return true;
-        return false;
+        int rh = f(node -> right);
+        if(rh == -1) return -1;
+
+        if(abs(lh - rh) > 1) return -1;
+        return max(lh , rh) + 1;
     }
 };
