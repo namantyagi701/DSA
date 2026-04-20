310658a6629f47242879d0dd787404d1e5e299a5
Naman Tyagi <namantyagi98665@gmail.com>
2026-04-20 13:36:48 +0530

    Time: 11 ms (23.80%) | Memory: 12.5 MB (30.84%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     int f(int i , int k , vector<int>& arr, int n , vector<int> &dp){
         if(i == n) return 0;
-        
         if(dp[i] != -1) return dp[i];
-        int len = 0;
-        int maxi = INT_MIN;
         int ans = INT_MIN;
         int sum = 0;
-        for(int j = i ; j < min(n , k + i ); j++){
-            len++;
-            maxi = max(maxi , arr[j]);
-            sum = len * maxi + f(j+1 , k , arr,n,dp);
-            ans = max(ans , sum);
+        int maxi = INT_MIN;
+        for(int j = i ; j < min(n ,i + k) ; j++){
+          maxi = max(maxi , arr[j]);
+          int len = (j - i + 1);
+          sum = len * maxi + f(j + 1 , k , arr , n , dp);
+          ans = max(ans ,sum);
         }
         return dp[i] = ans;
     }
