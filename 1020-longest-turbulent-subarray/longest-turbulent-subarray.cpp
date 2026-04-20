9bd32501abfecc07f3696e783c0d88e111522f15
Naman Tyagi <namantyagi98665@gmail.com>
2026-04-20 16:39:31 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (25.97%) - LeetSync

--- 1020-longest-turbulent-subarray/longest-turbulent-subarray.cpp
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.cpp
@@ -1,42 +1,19 @@
 class Solution {
 public:
-    int f(int i, bool flag, vector<int>& arr, vector<vector<int>>& dp){
-        if(i >= arr.size()) return 0;
-
-        if(dp[i][flag] != -1) return dp[i][flag];
-
-        int take = 0;
-
-        if(flag){
-            if(arr[i-1] < arr[i]) 
-                take = 1 + f(i + 1, !flag, arr, dp);
-            else 
-                return dp[i][flag] = 0;
-        }
-        else{
-            if(arr[i-1] > arr[i]) 
-                take = 1 + f(i + 1, !flag, arr, dp);
-            else 
-                return dp[i][flag] = 0;
-        }
-
-        return dp[i][flag] = take;
-    }
-
     int maxTurbulenceSize(vector<int>& arr) {
-        int n = arr.size();
-
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-
-        int maxi = 0;
-
-        for(int i = 1; i < n; i++){
-            int ans1 = f(i, true, arr, dp);
-            int ans2 = f(i, false, arr, dp);
-
-            maxi = max(maxi, max(ans1, ans2));
+        if(arr.size()==1) return 1;
+        int maxlen=0;
+        int l=0,r=1;
+        
+        while(r<arr.size()){
+            if(arr[r-1]==arr[r]) l=r;
+            else if(r==1 ||
+            arr[r-2]> arr[r-1] and arr[r-1]< arr[r]||
+            arr[r-2]< arr[r-1] and arr[r-1]> arr[r]){}
+            else l=r-1;
+            maxlen=max(maxlen,r-l+1);
+            r++;
         }
-
-        return 1 + maxi;
+        return maxlen;
     }
 };
