0257b79408984bc1394382c49bb848672c79a531
Naman Tyagi <namantyagi98665@gmail.com>
2026-04-20 16:37:04 +0530

    Time: 89 ms (5.39%) | Memory: 71.2 MB (5.12%) - LeetSync

--- 1020-longest-turbulent-subarray/longest-turbulent-subarray.cpp
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.cpp
@@ -1,28 +1,42 @@
 class Solution {
 public:
-    int f(int i , bool flag , vector<int> &arr){
+    int f(int i, bool flag, vector<int>& arr, vector<vector<int>>& dp){
         if(i >= arr.size()) return 0;
+
+        if(dp[i][flag] != -1) return dp[i][flag];
+
         int take = 0;
-        int maxi = 0;
+
         if(flag){
-            if(arr[i-1] < arr[i]) take = 1 + f(i + 1 , !flag , arr);
-            else return 0;
+            if(arr[i-1] < arr[i]) 
+                take = 1 + f(i + 1, !flag, arr, dp);
+            else 
+                return dp[i][flag] = 0;
         }
         else{
-            if(arr[i-1] > arr[i]) take = 1 + f(i + 1, !flag , arr);
-            else return 0;
+            if(arr[i-1] > arr[i]) 
+                take = 1 + f(i + 1, !flag, arr, dp);
+            else 
+                return dp[i][flag] = 0;
         }
-        return take;
+
+        return dp[i][flag] = take;
     }
+
     int maxTurbulenceSize(vector<int>& arr) {
         int n = arr.size();
+
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+
         int maxi = 0;
-        int ans = 0;
-        for(int i = 1 ; i < n ; i++){
-            int ans1 = f(i , true , arr);
-            maxi = max(maxi , f(i , false ,arr));
-            maxi=max(maxi,ans1);
+
+        for(int i = 1; i < n; i++){
+            int ans1 = f(i, true, arr, dp);
+            int ans2 = f(i, false, arr, dp);
+
+            maxi = max(maxi, max(ans1, ans2));
         }
-        return 1+maxi;       
+
+        return 1 + maxi;
     }
 };
