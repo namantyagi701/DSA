2a0607014385602a12ee4c4ea491bcc43009cd32
Naman Tyagi <namantyagi98665@gmail.com>
2026-04-24 22:03:44 +0530

    Time: 3 ms (6.15%) | Memory: 10.7 MB (42.60%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    int f(int i ,vector<int>& arr , vector<int> &dp){
-        if(i >= arr.size()) return 0;
-        
-        if(dp[i] != -1) return dp[i];
-        int notTake = f(i+1 , arr ,dp);
-        int take = arr[i] + f(i + 2 , arr , dp);
-
-        return dp[i] = max(take , notTake);
-    }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n+1 , -1);
-        return f(0 , nums ,dp);
+        vector<int>dp(n+2, 0);
+        
+        for(int i = n-1 ; i >=0 ; i--){
+            int notTake = dp[i+1];
+            int take = nums[i] + dp[i+2];
+
+            dp[i] = max(take , notTake);
+        }
+
+        return dp[0];
     }
 };
