28ac41330b7b3bf388115190b31cfbf5b311f143
Naman Tyagi <namantyagi98665@gmail.com>
2026-04-25 10:46:19 +0530

    Time: 12 ms (52.57%) | Memory: 17 MB (22.28%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,18 +1,23 @@
 class Solution {
-private:
-    int f(int i , int j , string &s1 , string &s2 ,vector<vector<int>> &dp){
-        if(i == 0 || j == 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s1[i-1] == s2[j-1]) return dp[i][j] = 1 + f(i-1 , j-1 , s1 ,s2 , dp);
-        return dp[i][j] = max(f(i-1 , j , s1 ,s2 , dp) , f(i , j-1 , s1 ,s2 , dp));
-    }
 public:
-    int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-        vector<vector<int>> dp(n+1 , vector<int>(m+1 , -1));
-        int k = f(n , m , word1 , word2 , dp);
-        int ans = (n+m) - (2 *k);
-        return ans;
+    int minDistance(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+        int ts = n+ m;
+
+        vector<vector<int>>dp(n+1 , vector<int>(m+1 , 0));
+        int ans = 0;
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= m ; j++){
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                    ans = max(ans , dp[i][j]);
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+                }
+            }
+        }
+        return ts - (2 *ans);
     }
 };
