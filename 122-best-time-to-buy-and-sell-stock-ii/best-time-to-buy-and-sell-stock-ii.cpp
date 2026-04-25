1ec2019249d2e91cd609478d0a395d280bc3b83e
Naman Tyagi <namantyagi98665@gmail.com>
2026-04-25 14:27:04 +0530

    Time: 15 ms (12.03%) | Memory: 28.2 MB (6.14%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,40 +1,22 @@
 class Solution {
 public:
-    int f(int i, int buy, vector<int>& prices, vector<vector<int>>& dp) {
-        if (i == prices.size())
-            return 0;
-
-        if (dp[i][buy] != -1)
-            return dp[i][buy];
-        int profit = 0;
-        if (buy) {
-            profit = max(-prices[i] + f(i + 1, 0, prices, dp),
-                         0 + f(i + 1, 1, prices, dp));
-        } else {
-            profit = max(prices[i] + f(i + 1, 1, prices, dp),
-                         0 + f(i + 1, 0, prices, dp));
-        }
-        return dp[i][buy] = profit;
+    int f(int i , bool flag , vector<int> & prices , vector<vector<int>> &dp){
+        if(i == prices.size()) return 0;
+        
+        if(dp[i][flag] != -1) return dp[i][flag];
+        int nt = f(i+1, flag , prices, dp);
+        int take = 0;
+        if(flag){
+          take = -prices[i] + f( i + 1 , false , prices,dp);  
+        } 
+        else{
+          take = prices[i] + f(i +1 , true , prices,dp);
+        }   
+        return dp[i][flag] = max(take , nt);    
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        // vector<vector<int>> dp(n + 1, vector<int>(2, 0));
-        vector<int>curr(2 , 0) , next(2,0);
-        int profit = 0;
-        next[1] = 0;
-        next[0] = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            for(int j = 0 ; j <= 1 ; j++){
-            if (j) {
-                curr[j] = max(-prices[i] + next[0],
-                             0 + next[1]);
-            } else {
-                curr[j] = max(prices[i] + next[1],
-                             0 + next[0]);
-            }
-            }
-            next = curr;
-        }
-        return next[1];
+        vector<vector<int>> dp(n , vector<int>(2 , -1));
+        return f(0 , true , prices , dp);
     }
 };
