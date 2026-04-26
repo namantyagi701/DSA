acd7dc148945161efa1784ae2566c481f817290f
Naman Tyagi <namantyagi98665@gmail.com>
2026-04-26 09:19:49 +0530

    Time: 203 ms (32.84%) | Memory: 13.8 MB (79.90%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int f(int i , int j , vector<int> &nums , vector<vector<int>> &dp){
+    int f(int i , int j , vector<int>& arr , vector<vector<int>>& dp){
         if(i > j) return 0;
-         
+        
         if(dp[i][j] != -1) return dp[i][j];
-        int maxi = INT_MIN;
-        for(int idx = i ; idx <=j ; idx++){
-            int cost = nums[i-1] * nums[idx] * nums[j + 1] + f(i , idx -1 , nums ,dp) + f(idx + 1 , j , nums,dp);
-            maxi = max(maxi , cost);
+        int maxi = -1e9;
+        for(int k = i ; k <= j ; k++){
+            int cost = arr[i-1] * arr[k] * arr[j+1] + f(i , k-1 ,arr ,dp) + f(k+1 , j , arr , dp);
+            maxi = max(cost , maxi);
         }
         return dp[i][j] = maxi;
     }
     int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n + 1 , vector<int>(n+ 1 , -1));
         nums.push_back(1);
         nums.insert(nums.begin() , 1);
-        int n = nums.size();
-        vector<vector<int>> dp(n-1 , vector<int>(n-1 , -1));
-        return f(1 , n -2 , nums ,dp);
+        return f(1 , n , nums , dp);
     }
 };
