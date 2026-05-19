3345a8d868fa8fdbb54dbcc30408bb279df78a49
Naman Tyagi <namantyagi98665@gmail.com>
2026-05-20 02:57:33 +0530

    Time: 24 ms (78.12%) | Memory: 17.4 MB (45.91%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
+    int dx[4] = {-1, 0, 1, 0};
+    int dy[4] = {0, -1, 0, 1};
+    void dfs(int row, int col, vector<vector<char>>& grid,vector<vector<int>>& vis) {
+        vis[row][col] = 1;
+        for(int i = 0 ; i < 4 ; i++){
+            int nrow = row + dx[i];
+            int ncol = col + dy[i];
+            if(nrow >= 0 && nrow < grid.size() && ncol >= 0 && ncol < grid[0].size() && vis[nrow][ncol] == 0 && grid[nrow][ncol] == '1'){
+                dfs(nrow , ncol , grid , vis);
+            }
+        }
+    }
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> vis(n , vector<int>(m , 0));
+        vector<vector<int>> vis(n, vector<int>(m, 0));
 
-        queue<pair<int , int>>q;
-        int dx[] = {-1 , 0 , 1 , 0};
-        int dy[] = {0 , 1 , 0 , -1};
         int count = 0;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(vis[i][j] == 0 && grid[i][j] == '1'){
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+               if (grid[i][j] == '1' && vis[i][j] != 1) {
                     count++;
-                    q.push({i , j});
-                    while(!q.empty()){
-                        auto curr = q.front();
-                        int row = curr.first;
-                        int col = curr.second;
-                        q.pop();
-                        vis[row][col] = 1;
-                        for(int ii = 0 ; ii < 4 ; ii++){
-                            int nRow = row + dx[ii];
-                            int nCol = col + dy[ii];
-                            if(nRow >= 0 && nRow < n && nCol >=0 && nCol < m && vis[nRow][nCol] == 0 && grid[nRow][nCol] == '1'){
-                                vis[nRow][nCol] = 1;
-                                q.push({nRow , nCol});
-                            }
-                        }
-                    }
+                    dfs(i, j, grid, vis);
                 }
             }
         }
