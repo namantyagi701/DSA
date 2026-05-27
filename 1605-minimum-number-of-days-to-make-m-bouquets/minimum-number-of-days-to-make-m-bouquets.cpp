989e7a446950678088aab9690c50c64fbfe3b755
Naman Tyagi <namantyagi98665@gmail.com>
2026-05-27 17:22:22 +0530

    Time: 16 ms (91.01%) | Memory: 70.6 MB (11.51%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,44 +1,37 @@
 class Solution {
 public:
-    int bloom(vector<int> &arr , int curr , int k){
+    int calc(vector<int>& arr, int curr, int k) {
         int n = arr.size();
         int ans = 0;
         int cnt = 0;
-        for(int i = 0 ; i < n ; i++){
-            if(arr[i] <= curr){
+        for (int i = 0; i < n; i++) {
+            if (arr[i] <= curr) {
                 cnt++;
-                if(cnt == k) {
+                if (cnt == k) {
                     ans++;
                     cnt = 0;
-                    }
-            } 
-            else{
+                }
+            } else {
                 cnt = 0;
             }
         }
         return ans;
     }
-
     int minDays(vector<int>& arr, int m, int k) {
-        int n = arr.size();
-        long long temp = 1LL * m *k;
-        if(temp > n) return -1;
-        int ans = 0;
-        int low = INT_MAX;
-        int high = INT_MIN;
-        for(int i = 0 ; i < n ; i++){
-            low = min(low , arr[i]);
-            high = max(high , arr[i]);
+        int low = 1;
+        int high = arr[0];
+        for (int i = 0; i < arr.size(); i++) {
+            high = max(arr[i], high);
         }
-        while(low <= high){
-            int mid = (low + high)/2;
+        int ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
 
-            int x = bloom(arr , mid , k);
-            if(x >= m){
-               ans = mid;
-               high = mid - 1;
-            }
-            else {
+            int curr = calc(arr, mid, k);
+            if (curr >= m) {
+                ans = mid;
+                high = mid - 1;
+            } else {
                 low = mid + 1;
             }
         }
