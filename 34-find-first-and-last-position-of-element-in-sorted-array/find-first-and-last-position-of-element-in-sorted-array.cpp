a23c73f80bcff5008e314b905e0b4a60f330122f
Naman Tyagi <namantyagi98665@gmail.com>
2026-05-27 13:36:05 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (88.10%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,49 +1,41 @@
 class Solution {
 public:
-    int lb(vector<int>& arr, int target){
+    int lb(vector<int>& arr, int k) {
         int low = 0;
-        int high = arr.size()-1;
-        int ans = -1;
-        while(low <= high){
-            int mid = (low + high)/2;
-            if(arr[mid] >= target){
+        int high = arr.size() - 1;
+        int ans = arr.size();
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] >= k) {
                 ans = mid;
-                high = mid -1;
-            }
-            else{
+                high = mid - 1;
+            } else {
                 low = mid + 1;
             }
         }
         return ans;
     }
 
-    int ub(vector<int>& arr, int target){
+    int ub(vector<int>& arr, int k) {
         int low = 0;
-        int high = arr.size()-1;
-        int ans = -1;
-        while(low <= high){
-            int mid = (low + high)/2;
-            if(arr[mid] > target){
+        int high = arr.size() - 1;
+        int ans = arr.size();
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] > k) {
                 ans = mid;
-                high = mid -1;
-            }
-            else{
+                high = mid - 1;
+            } else {
                 low = mid + 1;
             }
         }
         return ans;
     }
-    
+
     vector<int> searchRange(vector<int>& nums, int target) {
-        if(nums.size() == 0) return {-1 , -1};
-        int f_i = lb(nums , target);
-        int l_i = ub(nums , target);
-       
-        if(f_i == -1 || nums[f_i] != target) return {-1 , -1};
-        if(l_i == -1){
-            l_i = nums.size()-1;
-        }
-        else l_i = l_i -1;
-        return {f_i , l_i};
+        int first = lb(nums, target);
+        int last = ub(nums, target);
+        if(first == nums.size() ||  nums[first] != target) return {-1 , -1};
+        else return {first , last-1};
     }
 };
