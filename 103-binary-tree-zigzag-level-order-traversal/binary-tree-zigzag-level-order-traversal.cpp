cae737b0c058d6b9b7275596454d70c826a82be5
Naman Tyagi <namantyagi98665@gmail.com>
2026-05-31 23:17:34 +0530

    Time: 2 ms (12.69%) | Memory: 15 MB (77.05%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -1,36 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        if (!root) return {};
-
-        vector<vector<int>> ans;
-        queue<TreeNode*> q;
+        vector<vector<int>>ans;
+        if(!root) return ans;
+        queue<TreeNode*>q;
         q.push(root);
-
-        bool flag = false; // false = left→right, true = right→left
-
-        while (!q.empty()) {
+        int bit = 0;
+        while(!q.empty()){
+            vector<int>temp;
             int size = q.size();
-            vector<int> level(size); // pre-size vector
-
-            for (int i = 0; i < size; i++) {
-                TreeNode* curr = q.front();
+            for(int i = 0 ; i < size ; i++){
+                auto curr = q.front();
                 q.pop();
-
-                // 🔥 key logic
-                if (flag)
-                    level[size - 1 - i] = curr->val;
-                else
-                    level[i] = curr->val;
-
-                if (curr->left) q.push(curr->left);
-                if (curr->right) q.push(curr->right);
+                temp.push_back(curr -> val);
+                if(curr -> left) q.push(curr -> left);
+                if(curr -> right) q.push(curr -> right);
             }
-
-            ans.push_back(level);
-            flag = !flag;
+            if(bit == 0){
+                ans.push_back(temp);
+                bit = 1;
+            }
+            else{
+                bit = 0;
+                reverse(temp.begin() , temp.end());
+                ans.push_back(temp);
+            }
         }
-
         return ans;
     }
 };
