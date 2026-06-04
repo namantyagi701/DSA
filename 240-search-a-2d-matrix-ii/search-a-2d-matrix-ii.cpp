774413d482275e4852673f92c8050688671d853d
Naman Tyagi <namantyagi98665@gmail.com>
2026-06-04 13:56:56 +0530

    Time: 35 ms (98.58%) | Memory: 18.8 MB (37.22%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = 0;
+        int col = matrix[0].size() - 1;
+        while(col >= 0 && row < matrix.size()){
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] > target) col--;
+            else row++;
+        }
+        return false;
+    }
+};
