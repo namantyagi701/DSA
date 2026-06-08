b4d26b991d19a61437f756538e769e4d81d8a69e
Naman Tyagi <namantyagi98665@gmail.com>
2026-06-08 11:30:43 +0530

    Time: 80 ms (64.91%) | Memory: 56.2 MB (55.38%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int f(vector<int>& nums, int k) {
-        int count = 0;
+    int f(vector<int> &nums , int k){
+        unordered_map<int , int>mpp;
+        if(k < 0) return 0;
         int l = 0;
-        unordered_map<int, int> mp;
-
-        for (int r = 0; r < nums.size(); r++) {
-            mp[nums[r]]++;
-            while (mp.size() > k) {
-                mp[nums[l]]--;
-                if (mp[nums[l]] == 0) {
-                    mp.erase(nums[l]);
-                }
+        int r = 0;
+        int cnt = 0;
+        int n = nums.size();
+        while(l < n && r < n){
+            mpp[nums[r]]++;
+            while(mpp.size() > k){
+                mpp[nums[l]]--;
+                if(mpp[nums[l]] == 0) mpp.erase(nums[l]);
                 l++;
             }
-            count += (r - l + 1);
+            cnt += (r - l +1);
+            r++;
         }
-        return count;
+        return cnt;
     }
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        int n = nums.size();
-        unordered_set<int> st;
-        return f(nums, k) - f(nums, k - 1);
+        return f(nums , k) - f(nums , k-1);
     }
 };
