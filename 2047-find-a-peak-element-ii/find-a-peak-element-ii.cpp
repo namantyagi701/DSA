53d6d8efbef07c794618dde25e6752ac1a39bce2
Naman Tyagi <namantyagi98665@gmail.com>
2026-06-09 19:45:12 +0530

    Time: 2 ms (10.54%) | Memory: 49.9 MB (99.78%) - LeetSync

---
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+
+        int i = 0, j = m - 1;
+        vector<int> arr(2);
+
+        while (i < n && j > 0) {
+            if (i < n - 1 && mat[i][j] < mat[i + 1][j]) {
+                i++;
+            }
+            else if (mat[i][j] < mat[i][j - 1]) {
+                j--;
+            }
+            else {
+                arr[0] = i;
+                arr[1] = j;
+                break;
+            }
+
+            cout << i << " " << j << endl;
+        }
+
+        if (j < m - 1 && mat[i][j + 1] > mat[i][j]) {
+            arr[1] = j + 1;
+        }
+
+        if (i < n - 1 && mat[i + 1][j] > mat[i][j]) {
+            arr[0] = i;
+        }
+
+        return arr;
+    }
+};
