166065f235771365ac82623b02a0ea950b822ebc
Naman Tyagi <namantyagi98665@gmail.com>
2026-06-10 16:55:23 +0530

    Time: 72 ms (5.30%) | Memory: 193.6 MB (5.17%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,49 +1,42 @@
 class Solution {
 public:
-    int mod = pow(10,9) + 7;
-    void nse(vector<int>& arr, vector<int>& next) {
-        stack<int> st;
-        int n = arr.size();
-        
-        for (int i = n-1; i >= 0; i--) {
-            while (!st.empty() && arr[st.top()] > arr[i]) {
-                st.pop();
-            }
-
-            next[i] = st.empty() ? n : st.top();
-            st.push(i);
-        }
-    }
-
-    void pse(vector<int>& arr, vector<int>& prev) {
-        stack<int> st;
-        int n = arr.size();
-
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && arr[st.top()] >= arr[i]) {
-                st.pop();
-            }
-
-            prev[i] = st.empty() ? -1 : st.top();
-            st.push(i);
-        }
-    }
-
-    int sumSubarrayMins(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> prev(n, -1);
-        vector<int> next(n, n);
-        nse(arr, next);
-        pse(arr, prev);
-        
-        long long sum = 0;
+    void pse(vector<int> &arr , vector<int> & prev){
+        stack<int>st;
         for(int i = 0 ; i < arr.size() ; i++){
-            int leftEl = i - prev[i];
-            int rightEl = (next[i] - i);
-            
-            sum += (1LL * leftEl * rightEl * arr[i]) % mod;
-            
+            while(!st.empty() && arr[st.top()] > arr[i]){
+                st.pop();
+            }
+            prev[i] = !st.empty() ? st.top() : -1;
+            st.push(i);
         }
-        return sum %mod;
+    }
+
+    void nse(vector<int> &arr , vector<int> & next){
+        stack<int>st;
+        int n = arr.size();
+        for(int i = n -1 ; i>= 0 ; i--){
+            while(!st.empty() && arr[st.top()] >= arr[i]){
+                st.pop();
+            }
+            next[i] = !st.empty() ? st.top() : n; 
+            st.push(i);
+        }
+    }
+    int sumSubarrayMins(vector<int>& arr) {
+        int MOD = 1e9 + 7;
+        int n = arr.size();
+        vector<int>prev(n , -1);
+        vector<int>next(n , n);
+        pse(arr , prev);
+        nse(arr , next);
+        long long sum = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            int left = i - prev[i];
+            int right = next[i] - i;
+
+            sum += (1LL * left * right * arr[i]) % MOD;
+        }
+        return sum % MOD;
     }
 };
