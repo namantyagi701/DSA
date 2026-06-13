4ce8db21cca9ea2c401f59607eb9f27774286c0e
Naman Tyagi <namantyagi98665@gmail.com>
2026-06-13 21:49:39 +0530

    Time: 23 ms (66.21%) | Memory: 18 MB (68.95%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,51 +1,28 @@
 class Solution {
 public:
-    // int f(int i, int k, vector<int>& arr, vector<vector<int>>& dp) {
-    //     if (i == 0) {
-    //         if (k == 0)
-    //             return 0;
-    //         if ((k % arr[i]) == 0) {
-    //             return k / arr[i];
-    //         }
-    //         return 1e7;
-    //     }
-
-    //     if (dp[i][k] != -1)
-    //         return dp[i][k];
-
-    //     int nt = f(i - 1, k, arr, dp);
-    //     int t = 1e7;
-    //     if (k >= arr[i]) {
-    //         t = 1 + f(i, k - arr[i], arr, dp);
-    //     }
-    //     return dp[i][k] = min(t, nt);
-    // }
     int coinChange(vector<int>& arr, int k) {
         // sort(arr.begin(), arr.end());
         int n = arr.size();
-        
-        vector<int>prev(k + 1 , 0);
         vector<int>curr(k + 1 , 0);
         for (int j = 0; j <= k; j++) {
             if ((j % arr[0]) == 0) {
-                prev[j] = (j / arr[0]);
+                curr[j] = (j / arr[0]);
             } else
-                prev[j] = 1e7;
+                curr[j] = 1e7;
         }
 
         for (int i = 1; i < n; i++) {
             for (int kk = 0; kk <= k; kk++) {
-                int nt = prev[kk];
+                int nt = curr[kk];
                 int t = 1e7;
                 if (kk >= arr[i]) {
                     t = 1 + curr[kk-arr[i]];
                 }
                 curr[kk] = min(t, nt);
             }
-            prev = curr;
         }
 
-        int ans = prev[k];
+        int ans = curr[k];
 
         if (ans == 1e7)
             return -1;
