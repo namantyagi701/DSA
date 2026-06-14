bd650b8cd7d3705c4cd63f8729eb676c3ec6f562
Naman Tyagi <namantyagi98665@gmail.com>
2026-06-14 19:34:05 +0530

    Time: 39 ms (28.60%) | Memory: 27.6 MB (14.01%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
-    // int f(int i, int j, string& s1, string& s2, vector<vector<int>>& dp) {
-    //     if (i == 0 || j == 0)
-    //         return 0;
-
-    //     if (dp[i][j] != -1)
-    //         return dp[i][j];
-    //     int match = 0;
-
-    //     if (s1[i - 1] == s2[j - 1]) {
-    //         match = 1 + f(i - 1, j - 1, s1, s2, dp);
-    //     } else {
-    //         match = max(f(i - 1, j, s1, s2, dp), f(i, j - 1, s1, s2, dp));
-    //     }
-    //     return dp[i][j] = match;
-    // }
+    int f(int i , int j , string &s1 , string &s2 , vector<vector<int>> &dp){
+        if(i == 0 && j == 0) return 0;
+        if(i == 0) return 0;
+        if(j == 0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        int match = INT_MIN;
+        int notmatch = INT_MIN;
+        if(s1[i-1] == s2[j-1]){
+            match = 1 + f(i-1 , j-1 , s1 ,s2,dp);
+        }
+        else{
+            notmatch = max(f(i-1 , j , s1 , s2,dp) , f(i , j-1 , s1 , s2 ,dp));
+        }
+        return dp[i][j] = max(match , notmatch);
+    }
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size();
         int m = text2.size();
-
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                int match = 0;
-
-                if (text1[i - 1] == text2[j - 1]) {
-                    match = 1 + dp[i-1][j-1];
-                } else {
-                    match =
-                        max(dp[i-1][j], dp[i][j-1]);
-                }
-                dp[i][j] = match;
-            }
-        }
-        return dp[n][m];
+        vector<vector<int>>dp(n+1 , vector<int>(m+1 , -1));
+        int ans = f(n , m , text1 , text2,dp);
+        if(ans < 0) return 0;
+        return ans;
     }
 };
