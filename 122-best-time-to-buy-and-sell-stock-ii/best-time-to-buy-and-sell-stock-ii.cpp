15844ee7eb3661f4537a3ec56af8cb4e8d90ba5e
Naman Tyagi <namantyagi98665@gmail.com>
2026-06-15 11:04:14 +0530

    Time: 11 ms (26.90%) | Memory: 28.1 MB (5.36%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int f(int i , bool flag , vector<int> & prices , vector<vector<int>> &dp){
-        if(i == prices.size()) return 0;
-        
-        if(dp[i][flag] != -1) return dp[i][flag];
-        int nt = f(i+1, flag , prices, dp);
+    int f(int i, vector<int> &prices , bool fl , vector<vector<int>> &dp){
+        if(i >= prices.size()) return 0;
+        if(dp[i][fl] != -1) return dp[i][fl];
+        int nt = f(i + 1 , prices , fl , dp);
         int take = 0;
-        if(flag){
-          take = -prices[i] + f( i + 1 , false , prices,dp);  
+        if(!fl){
+            take = -prices[i] + f(i+1 , prices , true ,dp);
         } 
         else{
-          take = prices[i] + f(i +1 , true , prices,dp);
-        }   
-        return dp[i][flag] = max(take , nt);    
+            take = prices[i] + f(i+1 , prices , false, dp);
+        }
+        return dp[i][fl] = max(take, nt);
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n , vector<int>(2 , -1));
-        return f(0 , true , prices , dp);
+        vector<vector<int>>dp(n , vector<int>(2 , -1));
+        return f(0 , prices , false , dp);
     }
 };
