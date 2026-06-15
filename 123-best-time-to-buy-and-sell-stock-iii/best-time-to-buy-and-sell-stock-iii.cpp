4365632164708d83869a9f406a53429c28213913
Naman Tyagi <namantyagi98665@gmail.com>
2026-06-15 11:20:33 +0530

    Time: 577 ms (5.06%) | Memory: 291.3 MB (5.10%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int f(int i , bool flag , vector<int> & prices , vector<vector<vector<int>>>&dp , int cnt){
-        if(i == prices.size()) return 0;
-        if(cnt == 0) return 0;
-        if(dp[i][flag][cnt] != -1) return dp[i][flag][cnt];
-        int take = 0;
-        if(flag){
-          take = max(-prices[i] + f( i + 1 , false , prices,dp, cnt) , 
-                                  f(i + 1 , true , prices , dp ,cnt));  
-        } 
+    int f(int i , int k , vector<int>&arr , bool fl ,vector<vector<vector<int>>> &dp){
+        if(i >= arr.size() || k < 1) return false;
+        
+        if(dp[i][k][fl] != -1) return dp[i][k][fl];
+        
+        int nt = f(i+1 , k , arr , fl,dp);
+        int t = 0;
+        if(!fl){
+            t = -arr[i] + f(i+1 , k , arr , true,dp);
+        }
         else{
-         take = max(prices[i] +f( i + 1 , true , prices,dp, cnt-1) , 
-                                  f(i + 1 , false , prices , dp ,cnt));  
-        }   
-        return dp[i][flag][cnt] = take;
+            t = arr[i] + f(i+1 , k-1 , arr , false , dp);
+        }
+        return dp[i][k][fl] = max(t , nt);
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n , vector<vector<int>>(2 , vector<int>(3 , -1)));
-        
-        return f(0 , true , prices , dp , 2);
+        vector<vector<vector<int>>>dp(n , vector<vector<int>>(3 , vector<int>(2 , -1)));
+        return f(0 , 2 , prices , false , dp);
     }
 };
