7ac064af3d4fd220a873253053b42d27af5ade25
Naman Tyagi <namantyagi98665@gmail.com>
2026-06-17 23:45:06 +0530

    Time: 1825 ms (5.01%) | Memory: 62.7 MB (12.88%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
-    int f(int i , int j , string &s , vector<vector<int>>&dp){
+    int isPalindrome(int i , int j , string &s){
+        if(i >= s.size() || j >= s.size()) return 0;
+        if(i == j) return 1;
+        while(i <= j){
+            if(s[i] != s[j]) return 0;
+            i++;
+            j--;
+        }
+        return 1;
+    }
+
+    int f(int i ,int j , string &s , vector<vector<int>> &dp){
         if(i > j) return 0;
 
         if(dp[i][j] != -1) return dp[i][j];
-        int mini = INT_MAX;
-        for(int k = i ; k <= j ; k++){
+
+        int mini = 1e9;
+        for(int k = j ; k >= i ; k--){
+            int curr = 0;
             if(isPalindrome(i , k , s)){
-                int cost = 1 + f(k + 1 , j  , s , dp);
-                mini = min(cost , mini);
+                curr = 1 + f(k+1 , j , s , dp);
+                mini = min(mini , curr);
             }
         }
         return dp[i][j] = mini;
     }
-    
-    bool isPalindrome(int i , int j , string &s){
-        while(i <= j){
-            if(s[i] != s[j]) return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
 
     int minCut(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n , vector<int>(n , -1));
-        return f(0 , n -1 , s , dp) -1;
+        if(n == 1) return 0;
+        vector<vector<int>>dp(n , vector<int>(n , -1));
+        return f(0 , n-1 , s , dp) -1;
     }
 };
