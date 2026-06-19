6a71bd2bfa92dd8ce73e872c382149581c03e6d2
Naman Tyagi <namantyagi98665@gmail.com>
2026-06-19 16:59:48 +0530

    Time: 4 ms (25.47%) | Memory: 21.1 MB (9.64%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    void dfs(int i , vector<int> &vis , vector<vector<int>> &adj){
-        vis[i] = 1;
-
-        for(auto it: adj[i]){
-            if(!vis[it])
-            dfs(it , vis , adj);
+    void dfs(int node , vector<vector<int>> &adj , vector<int>& vis){
+        vis[node] = 1;
+        for(int i = 0 ; i < adj[node].size() ; i++){
+            if(!vis[adj[node][i]])
+            dfs(adj[node][i] , adj , vis);
         }
     }
-
     int findCircleNum(vector<vector<int>>& arr) {
-        int nos = 0;
         int n = arr.size();
-        vector<int>vis(n , 0);
-
-        vector<vector<int>> adj(n);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i != j && arr[i][j] == 1)
+        vector<vector<int>>adj(n);
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(arr[i][j] == 1 && i != j){
                     adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
             }
         }
-
-        for (int i = 0; i < n; i++) {
-            if (!vis[i]) {
-                nos++;
-                dfs(i, vis, adj);
+        vector<int>vis(n , 0);
+        int cnt = 0;
+        for(int i = 0 ; i < adj.size() ; i++){
+            if(vis[i] != 1){
+                cnt++;
+                dfs(i , adj , vis);
             }
         }
-        return nos;
+        return cnt;
     }
 };
