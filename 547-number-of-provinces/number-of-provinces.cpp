0d322be32ad9107933ef870e6e1e6c4c9b1138ad
Naman Tyagi <namantyagi98665@gmail.com>
2026-06-19 17:12:58 +0530

    Time: 3 ms (37.86%) | Memory: 19.5 MB (72.86%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -3,7 +3,7 @@ public:
     void dfs(int node , vector<vector<int>> &arr , vector<int>& vis){
         vis[node] = 1;
         for(int i = 0 ; i < arr.size() ; i++){
-            if(arr[node][i] == 1 && !vis[i])
+            if(arr[node][i] == 1 && !vis[i] && node != i)
                dfs(i , arr , vis);
         }
     }
