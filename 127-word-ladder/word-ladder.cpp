97f6347ead84e527dc6f27a70728ca254bfa953b
Naman Tyagi <namantyagi98665@gmail.com>
2026-06-21 13:03:20 +0530

    Time: 1012 ms (9.42%) | Memory: 40.5 MB (9.39%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string>st;
-        for(int i = 0 ; i < wordList.size() ; i++){
+    int ladderLength(string beginWord, string endWord,
+                     vector<string>& wordList) {
+        unordered_set<string> st;
+        for (int i = 0; i < wordList.size(); i++) {
             st.insert(wordList[i]);
         }
-        queue<pair<string,int>>q;
-        q.push({beginWord , 1});
-
-        while(!q.empty()){
-            auto curr = q.front();
+        queue<pair<int, string>> q;
+        q.push({1, beginWord});
+        while (!q.empty()) {
+            auto ele = q.front();
             q.pop();
-            string word = curr.first;
-            int len = curr.second;
-            if(word == endWord) return len;
+            int cnt = ele.first;
+            string word = ele.second;
+            if(word == endWord) return cnt;
             st.erase(word);
-
-            for(int i = 0 ; i < word.size() ; i++){
-                string org = word;
-                for(char k = 'a' ; k <= 'z' ; k++){
-                    word[i] = k;
-                    if(st.find(word) != st.end()){
-                        q.push({word , len+1});
+            for (int i = 0; i < word.size(); i++) {
+                char og = word[i];
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    word[i] = ch;
+                    if(st.count(word)){
+                        q.push({cnt + 1 , word});
                     }
                 }
-                word = org;
+                word[i] = og;
             }
         }
         return 0;
