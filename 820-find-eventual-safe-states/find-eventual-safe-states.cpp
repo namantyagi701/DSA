ccdb5ab9b170016834d8af2a420352739002a046
Naman Tyagi <namantyagi98665@gmail.com>
2026-06-22 12:38:01 +0530

    Time: 50 ms (43.12%) | Memory: 66.3 MB (19.41%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -2,38 +2,31 @@ class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<vector<int>> adj(n);
-        map<int, vector<int>> mpp;
-        for (int i = 0; i < n; i++) {
-            for (auto it : graph[i]) {
-                adj[i].push_back(it);
-                mpp[it].push_back(i);
+        vector<vector<int>>arr(n);
+        vector<int>outorder(n,0);
+        for(int i = 0 ; i < n ; i++){
+            for(auto it: graph[i]){
+                arr[it].push_back(i);
+                outorder[i]++;
             }
         }
-
-        // outdgree
-
-        vector<int> out(n, 0);
-        queue<int> q;
-        for (int i = 0; i < n; i++) {
-            int size = adj[i].size();
-            out[i] = size;
-            if (size == 0) {
+        queue<int>q;
+        for(int i = 0 ; i < n ; i++){
+            if(outorder[i] == 0){
                 q.push(i);
             }
         }
-        vector<int> ans;
-        while (!q.empty()) {
-            int top = q.front();
+        vector<int>ans;
+        while(!q.empty()){
+            int ele = q.front();
+            ans.push_back(ele);
             q.pop();
-            ans.push_back(top);
-            for (auto it : mpp[top]) {
-                out[it]--;
-                if (out[it] == 0)
-                    q.push(it);
+            for(auto it : arr[ele]){
+                outorder[it]--;
+                if(outorder[it] == 0) q.push(it);
             }
         }
-        sort(ans.begin(), ans.end());
+        if(ans.size() > 1) sort(ans.begin() ,ans.end());
         return ans;
     }
 };
