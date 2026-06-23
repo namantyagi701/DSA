944938df83ba1f372ab86e04882c6e1ae89515c5
Naman Tyagi <namantyagi98665@gmail.com>
2026-06-23 11:20:39 +0530

    Time: 59 ms (15.45%) | Memory: 36.8 MB (14.09%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -5,7 +5,7 @@ public:
         if (grid[n - 1][n - 1] != 0 || grid[0][0] != 0)
             return -1;
         set<pair<int, pair<int, int>>> st;
-        vector<vector<int>>minDis(n , vector<int>(n , INT_MAX));
+        vector<vector<int>> minDis(n, vector<int>(n, INT_MAX));
         st.insert({1, {0, 0}});
         minDis[0][0] = 1;
         int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
@@ -17,18 +17,23 @@ public:
             int row = ele.second.first;
             int col = ele.second.second;
 
-            for(int i = 0 ; i < 8 ; i++){
+            for (int i = 0; i < 8; i++) {
                 int nRow = row + dx[i];
                 int nCol = col + dy[i];
-                if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < n && grid[nRow][nCol] == 0 && grid[nRow][nCol] == 0){
-                    if(minDis[nRow][nCol] > 1 + dist){
+                if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < n &&
+                    grid[nRow][nCol] == 0 && grid[nRow][nCol] == 0) {
+                    if (minDis[nRow][nCol] > 1 + dist) {
+                        if(minDis[nRow][nCol] != INT_MAX) {
+                            st.erase({minDis[nRow][nCol], {nRow, nCol}});
+                        }
                         minDis[nRow][nCol] = 1 + dist;
-                        st.insert({1+dist , {nRow , nCol}});
+                        st.insert({1 + dist, {nRow, nCol}});
                     }
                 }
             }
         }
-        if(minDis[n-1][n-1] == INT_MAX) return -1;
-        return minDis[n-1][n-1];
+        if (minDis[n - 1][n - 1] == INT_MAX)
+            return -1;
+        return minDis[n - 1][n - 1];
     }
 };
