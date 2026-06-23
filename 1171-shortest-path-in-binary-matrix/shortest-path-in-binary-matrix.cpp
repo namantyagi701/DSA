2c90849a1cb43fa368e6e527d99ddc3b7dffd62f
Naman Tyagi <namantyagi98665@gmail.com>
2026-06-23 11:28:56 +0530

    Time: 25 ms (34.66%) | Memory: 26.3 MB (27.42%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -4,36 +4,32 @@ public:
         int n = grid.size();
         if (grid[n - 1][n - 1] != 0 || grid[0][0] != 0)
             return -1;
-        set<pair<int, pair<int, int>>> st;
-        vector<vector<int>> minDis(n, vector<int>(n, INT_MAX));
-        st.insert({1, {0, 0}});
+        queue<pair<int, pair<int, int>>> q;
+        vector<vector<int>> minDis(n, vector<int>(n, 0));
+        q.push({1, {0, 0}});
         minDis[0][0] = 1;
         int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
         int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-        while (!st.empty()) {
-            auto ele = *st.begin();
-            st.erase(st.begin());
+        while (!q.empty()) {
+            auto ele = q.front();
+            q.pop();
             int dist = ele.first;
             int row = ele.second.first;
             int col = ele.second.second;
+            if(row == n-1 && col == n-1) return dist;
 
             for (int i = 0; i < 8; i++) {
                 int nRow = row + dx[i];
                 int nCol = col + dy[i];
                 if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < n &&
-                    grid[nRow][nCol] == 0 && grid[nRow][nCol] == 0) {
-                    if (minDis[nRow][nCol] > 1 + dist) {
-                        if(minDis[nRow][nCol] != INT_MAX) {
-                            st.erase({minDis[nRow][nCol], {nRow, nCol}});
-                        }
-                        minDis[nRow][nCol] = 1 + dist;
-                        st.insert({1 + dist, {nRow, nCol}});
+                    grid[nRow][nCol] == 0) {
+                    if (minDis[nRow][nCol] == 0) {
+                        minDis[nRow][nCol] = 1;
+                        q.push({1 + dist , {nRow, nCol}});
                     }
                 }
             }
         }
-        if (minDis[n - 1][n - 1] == INT_MAX)
-            return -1;
-        return minDis[n - 1][n - 1];
+        return -1;
     }
 };
