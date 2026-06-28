1c344de753f53ba2e4241fbcb420483a08a4bea0
Naman Tyagi <namantyagi98665@gmail.com>
2026-06-28 22:40:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (20.62%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int temp = x ;
+        int rev = 0 ;
+        while(x != 0){
+            int last = x%10;
+            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && last > 7)) return 0;
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && last < -8)) return 0;
+            rev = rev *10 + last ;
+            x = x/10;
+        }
+    return rev ; 
+    }
+};
