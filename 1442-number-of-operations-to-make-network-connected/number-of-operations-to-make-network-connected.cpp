166a8ba912c3484b08b656cdc607cdfa5b16ca39
Naman Tyagi <namantyagi98665@gmail.com>
2026-06-29 16:22:34 +0530

    Time: 8 ms (69.86%) | Memory: 42.3 MB (74.55%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,36 +1,51 @@
+class Disjoint {
+    vector<int> size, parent;
+
+public:
+    Disjoint(int n) {
+        size.resize(n, 1);
+        parent.resize(n);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int find_ul_p(int node){
+        if(parent[node] == node) return node;
+        return parent[node] = find_ul_p(parent[node]);
+    }
+    void ubs(int u , int v){
+        int ul_u = find_ul_p(u);
+        int ul_v = find_ul_p(v);
+        if(ul_u == ul_v) return;
+        if(size[ul_u] < size[ul_v]){
+            parent[ul_u] = ul_v;
+            size[ul_v] += size[ul_u]; 
+        }
+        else{
+            parent[ul_v] = ul_u;
+            size[ul_u] += size[ul_v]; 
+        }
+    }
+};
 class Solution {
 public:
-    void dfs(int node , vector<vector<int>> &arr , vector<int> &vis){
-        vis[node] = 1;
-        for(auto it: arr[node]){
-            if(!vis[it]){
-                dfs(it , arr , vis);
-            }
-        }
-    }
-    int number(vector<vector<int>> &vect){
-        int n = vect.size();
-        int count = 0;
-        vector<int>vis(n , 0);
-            for(int i = 0 ; i < n ; i++){
-                if(!vis[i]){
-                    count++;
-                    dfs(i , vect , vis);
-                }
-            }
-
-        return count;
-    }
     int makeConnected(int n, vector<vector<int>>& arr) {
-        if(arr.size() < n - 1) return -1;
-        vector<vector<int>>vect(n);
-        for(int i = 0 ; i < arr.size() ; i++){
+        if (arr.size() < n - 1)
+            return -1;
+        Disjoint ds(n);
+        for (int i = 0; i < arr.size(); i++) {
             int u = arr[i][0];
             int v = arr[i][1];
-            vect[u].push_back(v);
-            vect[v].push_back(u);
+            ds.ubs(u,v);
         }
-        int nos = number(vect);
-        return nos-1;
+        int cnt = 1;
+        for(int i = 1 ;i < n ; i++){
+            if(ds.find_ul_p(i-1) != ds.find_ul_p(i)){
+                cnt++;
+                ds.ubs(i-1 , i);
+            }
+        }
+        return cnt - 1;
     }
 };
