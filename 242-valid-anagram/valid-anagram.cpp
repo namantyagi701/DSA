ccf8a3a60d4270c77cf3dc0354afab78ce859783
Naman Tyagi <namantyagi98665@gmail.com>
2026-07-01 22:06:50 +0530

    Time: 3 ms (46.29%) | Memory: 9.8 MB (49.56%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,14 +3,14 @@ public:
     bool isAnagram(string s, string t) {
         int n = s.size();
         int m = t.size();
-        if(n!= m) return false;
-
+        if(n != m) return false;
         sort(s.begin() , s.end());
-        sort(t.begin() , t.end());
-
-        for(int i = 0 ; i < n ; i++){
+        sort(t.begin(), t.end());
+        int i ;
+        for(i = 0 ; i < min(n , m) ; i++){
             if(s[i] != t[i]) return false;
-        }
+        } 
+        if(i < n || i < m) return false;
         return true;
     }
 };
