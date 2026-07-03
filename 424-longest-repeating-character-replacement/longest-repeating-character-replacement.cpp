c5420ab35aaf3d18547d8abf103022a27acdd1b8
Naman Tyagi <namantyagi98665@gmail.com>
2026-07-03 17:52:05 +0530

    Time: 4 ms (58.01%) | Memory: 10.9 MB (41.09%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-    int characterReplacement(string s, int t) {
-        int ans = 1;
+    int characterReplacement(string s, int x) {
+        int i = 0;
+        int j = 0;
         int n = s.size();
-        int k = t;
-        for(char ch = 'A' ; ch <= 'Z' ; ch++){
-            int l = 0;
-            int r = 0;
-            k = t;
-            while(l < n && r < n){
-               if(s[r] != ch) k--;
-               while(k < 0){
-                if(s[l] != ch) k++;
-                l++;
-               }
-               ans = max(ans , r - l + 1);
-               r++;
+        vector<int> freq(26);
+        int ans = 0;
+        while (i < n && j < n) {
+            freq[s[j] - 'A']++;
+            int maxFreq = 0;
+            for (int k = 0; k < 26; k++) {
+                if (freq[k] > maxFreq) {
+                    maxFreq = freq[k];
+                }
             }
-            if(ans == n) return n;
+            while (((j - i) + 1) - maxFreq > x) {
+                freq[s[i] - 'A']--;
+                i++;
+                maxFreq = 0;
+                for (int k = 0; k < 26; k++) {
+                    if (freq[k] > maxFreq) {
+                        maxFreq = freq[k];
+                    }
+                }
+            }
+            ans = max(ans, ((j - i) + 1));
+            j++;
         }
         return ans;
     }
