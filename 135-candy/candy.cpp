f1c2d4cc94f4788e2e0238cc6cbc7c97557581d4
Naman Tyagi <namantyagi98665@gmail.com>
2026-07-09 01:24:04 +0530

    Time: 958 ms (5.03%) | Memory: 23.4 MB (16.39%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int candy(vector<int>& arr) {
+        int n = arr.size();
+        vector<int>left(n , 0);
+        vector<int>right(n , 0);
+        //left 
+        for(int i = 1 ; i < n ; i++){
+            int j = i -1;
+            int k = i;
+            int cnt = 0;
+            while(j >= 0 && k >= 0 && arr[j] < arr[k]){
+                 cnt++;
+                 j--;
+                 k--;
+            }
+            left[i] = cnt;
+        }
+        for(int i = n-2 ; i >= 0 ; i--){
+            int j = i+1;
+            int k = i;
+            int cnt = 0;
+            while(j < n && k < n && arr[j] < arr[k]){
+                 cnt++;
+                 j++;
+                 k++;
+            }
+            right[i] = cnt;
+        }
+        int ans = 0;
+        for(int i = 0 ; i <  n ; i++){
+            ans += (max(left[i] , right[i]) + 1);
+        }
+        return ans;
+    }
+};
