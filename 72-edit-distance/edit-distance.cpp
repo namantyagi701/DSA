4ec00adba696372a44b97e89ebe6180a31b84b8a
Naman Tyagi <namantyagi98665@gmail.com>
2026-07-10 02:56:01 +0530

    Time: 315 ms (5.31%) | Memory: 405.3 MB (5.08%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    int f(int i , int j , string &s1 , string &s2 , vector<vector<int>> &dp){
-        if(i == 0 && j == 0) return 0;
-        if(i == 0 && j > 0) return j;
-        if(i > 0 && j == 0) return i;
-        if(j == 0 || i == 0) return 1e7;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(s1[i-1] == s2[j-1]){
-            return dp[i][j] = f(i-1 , j-1 , s1 ,s2,dp);
+    int f(int i , int j , string s , string t, vector<vector<int>> &dp){
+        if(i < 0 && j < 0) return 0;
+        if(i >= 0 && j < 0){
+            return i+1;
         }
-        return dp[i][j] = min({1 + f(i , j-1 , s1,s2 ,dp) , 1 + f(i-1 , j , s1,s2,dp) , 1 + f(i-1 , j-1 , s1 , s2,dp)});
+        if(i < 0 && j >= 0){
+            return j+1;
+        }
+        if(i == 0 && j == 0){
+            if(s[i] == t[j]) return 0;
+            return 1;
+        }
 
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s[i] == t[j]) return f(i-1 , j-1 , s , t, dp);
+        else{
+            return dp[i][j] = min({(1 + f(i-1, j , s , t,dp)),
+                         (1 + f(i-1 , j-1 , s , t,dp)),
+                         (1 + f(i , j-1 , s , t, dp))});
+        }
     }
     int minDistance(string word1, string word2) {
         int n = word1.size();
-        int m = word2.size(); 
-        vector<vector<int>>dp(n+1 , vector<int>(m + 1, -1));
-        int ans = f(n , m , word1 , word2 , dp);
-        if(ans == 1e7) return 0;
-        return ans;
+        int m = word2.size();
+        vector<vector<int>>dp(n , vector<int>(m , -1));
+        return f(n-1 , m-1 , word1,word2, dp);
     }
 };
