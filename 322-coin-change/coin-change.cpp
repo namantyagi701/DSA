45bba108b0d3545e133f8724ea1f41e4de9016df
Naman Tyagi <namantyagi98665@gmail.com>
2026-07-10 01:31:18 +0530

    Time: 43 ms (42.79%) | Memory: 53.2 MB (8.96%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    int coinChange(vector<int>& arr, int k) {
-        // sort(arr.begin(), arr.end());
-        int n = arr.size();
-        vector<int>curr(k + 1 , 0);
-        for (int j = 0; j <= k; j++) {
-            if ((j % arr[0]) == 0) {
-                curr[j] = (j / arr[0]);
-            } else
-                curr[j] = 1e7;
+    int f(int i , int amt , vector<int> &coins ,vector<vector<int>> &dp){
+        if(i == 0){
+           if(amt == 0) return 0;
+           if((amt % coins[i]) == 0) return amt / coins[i];
+           return 1e6;
         }
 
-        for (int i = 1; i < n; i++) {
-            for (int kk = arr[i]; kk <= k; kk++) {
-                int nt = curr[kk];
-                int t = 1 + curr[kk-arr[i]];
-                curr[kk] = min(t, nt);
-            }
+        if(dp[i][amt] != -1) return dp[i][amt];
+
+        long long nt = f(i-1 , amt , coins ,dp);
+        long long t = 1e6;
+        if(amt >= coins[i]){
+            t = 1 + f(i , amt - coins[i] , coins ,dp);
         }
-
-        int ans = curr[k];
-
-        if (ans == 1e7)
-            return -1;
-
+        return dp[i][amt] = min(t , nt);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        sort(coins.begin() ,coins.end());
+        int n = coins.size();
+        vector<vector<int>> dp(n+1 , vector<int>(amount+1 , -1));
+        int ans = f(n-1 , amount ,coins ,dp);
+        if(ans == 1e6) return -1;
         return ans;
     }
 };
