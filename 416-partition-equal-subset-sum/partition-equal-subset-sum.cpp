ea70b0174c517b552934aa7e672fa11540a860a0
Naman Tyagi <namantyagi98665@gmail.com>
2026-07-10 01:01:49 +0530

    Time: 133 ms (51.76%) | Memory: 111.3 MB (14.44%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    bool f(int i , int k , vector<int> &nums ,vector<vector<int>> &dp){
-        if(k == 0) return true;
-        if(i < 0 && k > 0) return false;
-        if(i == 0 && nums[i] == k) return true; 
+    bool f(int i , int k , vector<int> &nums, vector<vector<int>> &dp){
+        if(k == 0) return false;
+        if(i == 0){
+            if(nums[i] == k) return true;
+            return false;
+        }
         if(dp[i][k] != -1) return dp[i][k];
-        int notTake = f(i-1 , k , nums , dp);
-        int take = 0;
+        int nt = f(i-1 , k , nums ,dp);
+        int t = 0;
         if(nums[i] <= k){
-            take = f(i -1 , k - nums[i] , nums ,dp);
-        } 
-        return dp[i][k] = take || notTake;
+            t = f(i-1 , k -nums[i] , nums ,dp);
+        }
+        return dp[i][k] = t || nt;
     }
     bool canPartition(vector<int>& nums) {
-        int n = nums.size();
         int sum = 0;
+        int n = nums.size();
         for(int i = 0 ; i < n ; i++){
             sum += nums[i];
         }
-        int k = sum/2;
-        vector<vector<int>>dp(n , vector<int>(k + 1, -1));
         if(sum % 2 != 0) return false;
+        int k = sum /2;
+        vector<vector<int>>dp(n+1 , vector<int>(k+1 , -1));
         return f(n-1 , k , nums , dp);
     }
 };
