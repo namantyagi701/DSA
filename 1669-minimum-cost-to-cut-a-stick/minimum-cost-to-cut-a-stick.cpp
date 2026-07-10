bad9dcbd21eff41d89249b08c252bcb041d95fbf
Naman Tyagi <namantyagi98665@gmail.com>
2026-07-10 20:15:21 +0530

    Time: 38 ms (32.81%) | Memory: 14.1 MB (7.71%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int f(int i , int j , vector<int> &arr , vector<vector<int>> &dp){
-        if(i == j) return 0;
-        int mini = 1e9;
+    int f(int i , int j , vector<int> &arr , vector<vector<int>>&dp){
+        if(i > j) return 0;
+        
         if(dp[i][j] != -1) return dp[i][j];
-        for(int k = i ; k < j ; k++){
-            int curr = (arr[j] - arr[i-1]) + f(i , k , arr ,dp) + f(k+1  , j , arr,dp);
-            mini = min(mini , curr);
-        } 
+        int mini = 1e9;
+        for(int k = i ; k <= j ; k++){
+            int ans = arr[j+1] - arr[i-1] + f(i , k-1 , arr ,dp) + f(k+1 , j , arr ,dp);
+
+            mini = min(mini ,ans);
+        }
         return dp[i][j] = mini;
     }
     int minCost(int n, vector<int>& cuts) {
-        sort(cuts.begin() , cuts.end());
-        cuts.insert(cuts.begin(), 0);
         cuts.push_back(n);
-        int size = cuts.size();
-        vector<vector<int>>dp(size , vector<int>(size , -1));
-        return f(1 , size - 1 , cuts ,dp);
+        cuts.insert(cuts.begin() , 0);
+        sort(cuts.begin() , cuts.end());
+        int s = cuts.size();
+        vector<vector<int>> dp(s+1 , vector<int>(s+1 , -1));
+        return f(1 , s-2 , cuts , dp);
     }
 };
