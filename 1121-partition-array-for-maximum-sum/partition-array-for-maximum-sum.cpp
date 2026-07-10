091b0bbf32adbe7ace06fdbd40713b39cf68acc4
Naman Tyagi <namantyagi98665@gmail.com>
2026-07-11 02:49:17 +0530

    Time: 7 ms (51.86%) | Memory: 12.6 MB (31.81%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    int f(int i , int n, int k ,vector<int>& arr , vector<int> &dp){
-        if(i == n) return 0;
+    int f(int i, vector<int>& arr, int k , vector<int> &dp) {
+        if (i == arr.size())
+            return 0;
+        
         if(dp[i] != -1) return dp[i];
-        int maxi = INT_MIN;
         int ans = 0;
-        for(int x = i ; x < min(n , i + k) ; x++){
-            maxi = max(maxi , arr[x]);
-            int sum = (maxi *(x - i + 1)) + f(x + 1 , n , k , arr , dp);
-            ans = max(ans , sum);
+        int maxi = 0;
+
+        for (int x = i; x < arr.size() && x < i + k; x++) {
+            maxi = max(maxi, arr[x]);
+            int len = x - i + 1;
+            ans = max(ans, maxi * len + f(x + 1, arr, k, dp));
         }
-        return dp[i] = ans;        
+
+        return dp[i] = ans;
     }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int>dp(n , -1);
-        return f(0 , n , k , arr , dp);
+        vector<int> dp(n, -1);
+        return f(0, arr , k , dp);
     }
 };
