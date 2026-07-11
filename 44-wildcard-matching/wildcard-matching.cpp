4d8d59a574a04d6ce44a82f9599fac598c72392e
Naman Tyagi <namantyagi98665@gmail.com>
2026-07-11 13:58:56 +0530

    Time: 51 ms (54.92%) | Memory: 54.8 MB (12.57%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    bool f(int i , int j , string &s , string &p , vector<vector<int>> &dp){
+    bool f(int i , int j , string &s,string &p , vector<vector<int>>&dp){
         if(i == 0 && j == 0) return true;
+        if(j == 0 && i > 0) return false; 
         if(i == 0){
-            while(j > 0 && p[j-1] == '*'){
+            while(j != 0){
+                if(p[j-1] != '*') return false;
                 j--;
             }
-            if( j==0) return true;
-            return false;
+            return true;
         }
-        if(j == 0) return false;
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        if(s[i-1] == p [j-1] || p[j-1] == '?'){
-            return dp[i][j] = f(i-1 , j-1 , s , p , dp);
+        if(s[i-1] == p[j-1] || p[j-1] == '?'){
+            return dp[i][j] = f(i-1 , j-1 , s , p ,dp);
         }
-        else if(p[j-1] == '*') {
-            return dp[i][j] = f(i , j-1 , s , p , dp) || f(i-1 , j , s , p,dp) || f(i-1 , j-1,s , p,dp);
+        else if(p[j-1] == '*'){
+            return dp[i][j] = f(i-1 , j , s , p,dp) || f(i , j-1 , s , p,dp) || f(i-1 ,j-1 , s,p,dp);
         }
         else{
             return dp[i][j] = false;
@@ -26,10 +26,7 @@ public:
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-        if(n == 0 && m == 0) return true;
-
-        vector<vector<int>>dp(n+1 , vector<int>(m+1 , -1));
-
+        vector<vector<int>> dp(n+1 , vector<int>(m+1 , -1));
         return f(n , m , s , p , dp);
     }
 };
