5821704a0d0eda8f73b09dd164e7f51981503e82
Naman Tyagi <namantyagi98665@gmail.com>
2026-07-11 14:38:09 +0530

    Time: 429 ms (21.04%) | Memory: 11.7 MB (78.52%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    int f(int i , vector<int> &nums , int k , vector<vector<int>> &dp , int offset){
-        if (k + offset < 0 || k + offset >= dp[0].size()) return 0;
-        if(i == nums.size()-1){
-            if(nums[i] == 0 && k == 0) return 2;
-            if(k - nums[i] == 0 || k + nums[i]== 0 ) return 1;
+    int f(int i , vector<int> &nums , int k){
+        if(i == 0){
+            if(k == 0 && nums[i] == 0) return 2;
+            if(-nums[i] == k || nums[i] == k) return 1;
             return 0;
         }
-        if(i >= nums.size()) return 0;
     
-        if(dp[i][k + offset] != -1) return dp[i][k + offset];
-
-        int sub = f(i + 1 , nums , k + nums[i] , dp , offset);
-
+        int subs = f(i-1 , nums , k + nums[i]);
         int add = 0;
-        add = f(i + 1 , nums , k - nums[i] , dp , offset);
+        add = f(i-1 , nums , k - nums[i]);
 
-        return dp[i][k + offset] = sub + add;
+        return add + subs;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        int sum = 0;
-        for(auto it : nums) sum += it;
-        vector<vector<int>>dp(n , vector<int>(2 * sum + 1 , -1));
-        return f(0 , nums , target , dp , sum);
+        return f(n-1 , nums , target);
     }
 };
