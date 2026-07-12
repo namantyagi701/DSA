0d0ff79e36e68b272dc7cc5f26c8b55e1d0aa659
Naman Tyagi <namantyagi98665@gmail.com>
2026-07-12 12:09:17 +0530

    Time: 27 ms (65.68%) | Memory: 36.7 MB (48.17%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    int f(int i , int k , vector<int> &arr , vector<vector<int>> &dp){
-        if(i == 0){
-            if(k % arr[i] == 0) return 1;
-        }
+    int f(int i ,int k , vector<int> &coins , vector<vector<int>> &dp){
         if(k == 0) return 1;
-        if(k < 0 || i < 0) return 0;
-        
+        if(i == 0){
+            if((k % coins[i]) == 0) return 1;
+            return 0;
+        }
         if(dp[i][k] != -1) return dp[i][k];
-        int notTake = f(i-1 , k , arr , dp);
-        int take = 0;
-        if(arr[i] <= k) take = f(i , k-arr[i] , arr, dp);
+        int nt = f(i-1 , k , coins,  dp);
+        int t = 0;
+        if(coins[i] <= k) t = f(i, k - coins[i] , coins, dp);
 
-        return dp[i][k] = take + notTake;
+        return dp[i][k] = t + nt;
     }
-
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        sort(coins.begin() , coins.end());
-        vector<vector<int>> dp(n , vector<int>(amount + 1 , -1));
+        sort(coins.begin(), coins.end());
+        vector<vector<int>> dp(n , vector<int>(amount+1 , -1));
         return f(n-1 , amount, coins , dp);
     }
 };
