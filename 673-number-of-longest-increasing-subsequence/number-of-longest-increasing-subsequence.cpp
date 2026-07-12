878dd2355d22ff4c30c8dd352ff8cdbf1b98c5f1
Naman Tyagi <namantyagi98665@gmail.com>
2026-07-12 15:39:09 +0530

    Time: 27 ms (72.35%) | Memory: 17.5 MB (66.07%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -2,32 +2,32 @@ class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int>temp(n , 1);
-        vector<int>cnt(n , 1);
-        int maxi = INT_MIN;
-        // int idx = -1;
-        for(int i = 0 ; i < n ; i++){
+        int i = 0;
+        vector<int>lis(n , 1);
+        vector<int>nos(n , 1);
+        int maxlis = -1;
+        while(i < n){
             for(int j = 0 ; j < i ; j++){
-                if(nums[j] < nums[i]){
-                    if(temp[i] < 1 + temp[j]){
-                        temp[i] = 1 + temp[j];
-                        cnt[i] = cnt[j];
+                if(nums[i] > nums[j]){
+                    if(lis[j] + 1 > lis[i]){
+                        lis[i] = 1 + lis[j];
+                        nos[i] = nos[j] ;
                     }
-                    else if(temp[i] == 1 + temp[j]){
-                        cnt[i] += cnt[j];
+                    else if(lis[j] + 1 == lis[i]){
+                        nos[i] = nos[i] + nos[j];
                     }
                 }
+
             }
-            if(temp[i] > maxi){
-                maxi = temp[i];
+            maxlis = max(maxlis , lis[i]);
+            i++;
+        }
+        int ans = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(lis[i] == maxlis){
+                ans += nos[i];
             }
         }
-        int count = 0;
-            for(int i = 0 ; i < n ; i++){
-                if(temp[i] == maxi){
-                    count += cnt[i];
-                }
-            }
-        return count;
+        return ans;
     }
 };
