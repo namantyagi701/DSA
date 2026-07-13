6bb86e4494cb36002fddb9765210ff4c77df1ff1
Naman Tyagi <namantyagi98665@gmail.com>
2026-07-13 13:34:42 +0530

    Time: 6 ms (37.67%) | Memory: 15.3 MB (83.80%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -1,74 +1,68 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
-    void dfs(TreeNode* node, TreeNode* par,
-             unordered_map<TreeNode*, TreeNode*> &parent) {
-        if (!node)
-            return;
-
-        parent[node] = par;
-        dfs(node->left, node, parent);
-        dfs(node->right, node, parent);
-    }
-    void bfs(TreeNode* target, unordered_map<TreeNode*, TreeNode*>& parent,
-             int k, vector<int>& ans) {
-
+    void parent(TreeNode* node,
+                map<TreeNode*, TreeNode*>& mpp) {
+        
         queue<TreeNode*> q;
-        unordered_set<TreeNode*> vis;
-
-        q.push(target);
-        vis.insert(target);
+        q.push(node);
 
         while (!q.empty()) {
+            TreeNode* p = q.front();
+            q.pop();
 
-            int size = q.size();
-
-            if (k == 0) {
-                while (!q.empty()) {
-                    ans.push_back(q.front()->val);
-                    q.pop();
-                }
-                return;
+            if (p->left) {
+                mpp[p->left] = p;
+                q.push(p->left);
             }
 
-            while (size--) {
-
-                TreeNode* curr = q.front();
-                q.pop();
-
-                if (curr->left && !vis.count(curr->left)) {
-                    vis.insert(curr->left);
-                    q.push(curr->left);
-                }
-
-                if (curr->right && !vis.count(curr->right)) {
-                    vis.insert(curr->right);
-                    q.push(curr->right);
-                }
-
-                if (parent[curr] && !vis.count(parent[curr])) {
-                    vis.insert(parent[curr]);
-                    q.push(parent[curr]);
-                }
+            if (p->right) {
+                mpp[p->right] = p;
+                q.push(p->right);
             }
-
-            k--;
         }
     }
 
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        unordered_map<TreeNode*, TreeNode*> parent;
+    vector<int> distanceK(TreeNode* root,
+                          TreeNode* target,
+                          int k) {
+        
+        map<TreeNode*, TreeNode*> mpp;
+        parent(root, mpp);
+
+        queue<pair<TreeNode*, int>> q;
+        map<TreeNode*, bool> vis;
+
+        q.push({target, 0});
+        vis[target] = true;
+
         vector<int> ans;
-        dfs(root, NULL, parent);
-        bfs(target, parent, k, ans);
+
+        while (!q.empty()) {
+            TreeNode* node = q.front().first;
+            int dist = q.front().second;
+            q.pop();
+
+            if (dist == k) {
+                ans.push_back(node->val);
+                continue;
+            }
+
+            if (node->left && !vis[node->left]) {
+                vis[node->left] = true;
+                q.push({node->left, dist + 1});
+            }
+
+            if (node->right && !vis[node->right]) {
+                vis[node->right] = true;
+                q.push({node->right, dist + 1});
+            }
+
+            if (mpp.count(node) && !vis[mpp[node]]) {
+                vis[mpp[node]] = true;
+                q.push({mpp[node], dist + 1});
+            }
+        }
+
         return ans;
     }
 };
