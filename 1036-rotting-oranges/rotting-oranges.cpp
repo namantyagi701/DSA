76844f215a7fde86a3231e8c7768b6ae96305ec7
Naman Tyagi <namantyagi98665@gmail.com>
2026-07-15 12:46:35 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (29.23%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -7,37 +7,38 @@ public:
         queue<pair<int, pair<int, int>>> q;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
-                    vis[i][j] = 1;
-                    q.push({0, {i, j}});
-                }
+                if(grid[i][j] == 2)
+                q.push({0, {i, j}});
             }
         }
-        int time = 0;
-        int dx[] = {-1, 0, 1, 0};
-        int dy[] = {0, 1, 0, -1};
+        int maxTime = 0;
+        int dx[4] = {1, 0, -1, 0};
+        int dy[4] = {0, -1, 0, 1};
         while (!q.empty()) {
+            // int size = q.size();
             auto curr = q.front();
             q.pop();
-            time = curr.first;
-
+            int time = curr.first;
+            maxTime = max(maxTime , time);
             int row = curr.second.first;
             int col = curr.second.second;
-
-            for (int i = 0; i < 4; i++) {
-                int nRow = row + dx[i];
-                int nCol = col + dy[i];
-                if(nRow < n && nRow >= 0 && nCol < m && nCol >= 0 && grid[nRow][nCol] == 1 && vis[nRow][nCol] != 1){
-                    vis[nRow][nCol] = 1;
-                    q.push({time + 1 , {nRow , nCol}});
+            for(int k = 0 ; k < 4 ; k++){
+                int nRow = row + dx[k];
+                int nCol = col + dy[k];
+                if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && grid[nRow][nCol] == 1 && vis[nRow][nCol] == 0){
+                q.push({time+1,{nRow , nCol}});
+                vis[nRow][nCol] = 1;}
+            }
+        }
+        bool fl = true;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(grid[i][j] == 1 && vis[i][j] == 0){
+                    fl = false;
+                    break;
                 }
             }
         }
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(grid[i][j] == 1 && vis[i][j] == 0) return -1;
-            }
-        }
-        return time;
+        return fl ? maxTime : -1;
     }
 };
