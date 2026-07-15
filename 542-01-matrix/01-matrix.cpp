1e2ae9447a1b9a8601db3467fbdfb7587a043a9c
Naman Tyagi <namantyagi98665@gmail.com>
2026-07-15 19:59:53 +0530

    Time: 16 ms (66.81%) | Memory: 38.8 MB (30.21%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -3,36 +3,36 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-        vector<vector<int>> ans(n , vector<int>(m , 0));
-        vector<vector<int>> vis(n , vector<int>(m , 0));
-        queue<pair<int , pair<int , int>>>q;
+        vector<vector<int>>vis(n , vector<int>(m , 0));
+        vector<vector<int>>ans(n, vector<int>(m , 0));
+        queue<pair<int ,pair<int,int>>>q;
         for(int i = 0 ; i < n ; i++){
-            for(int j =  0 ; j < m ; j++){
+            for(int j = 0 ; j < m ; j++){
                 if(mat[i][j] == 0){
-                    vis[i][j] = 1;
-                    q.push({0,{i,j}});
-
+                q.push({0, {i , j}});
+                vis[i][j] = 1;
                 }
             }
-        }  
+        }
         int dx[] ={0 , 1 , 0 , -1};
         int dy[] ={1 , 0 , -1 , 0};
         while(!q.empty()){
-            auto ele = q.front();
+            auto curr = q.front();
+            int dist = curr.first;
+            int row = curr.second.first;
+            int col = curr.second.second;
             q.pop();
-            int cnt = ele.first;
-            int row = ele.second.first;
-            int col = ele.second.second;
             for(int i = 0 ; i < 4 ; i++){
                 int nRow = row + dx[i];
                 int nCol = col + dy[i];
-                if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && mat[nRow][nCol] != 0 && vis[nRow][nCol] != 1){
-                    vis[nRow][nCol] = 1;
-                    ans[nRow][nCol] = cnt + 1;
-                    q.push({cnt+1 , {nRow , nCol}});
+                if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && vis[nRow][nCol] == 0 && mat[nRow][nCol] != 0){
+                      q.push({dist+1 , {nRow , nCol}});
+                      ans[nRow][nCol] = dist+1;
+                      vis[nRow][nCol] = 1;
                 }
             }
-        } 
-        return ans;     
+        }
+
+        return ans;
     }
 };
