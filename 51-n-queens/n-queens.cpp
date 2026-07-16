4dd6e323a05f7c927b29ca4f4c604a9538dc8e36
Naman Tyagi <namantyagi98665@gmail.com>
2026-07-16 14:07:42 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (59.58%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,52 +1,45 @@
 class Solution {
 public:
-    bool isSafe(int row, int col, vector<string>& board) {
-        // upper row of the col
-        for (int i = row - 1; i >= 0; i--) {
-            if (board[i][col] == 'Q')
-                return false;
+    bool isSafe(int row , int col , vector<string> &board){
+        for(int i = row - 1 ; i >= 0 ; i--){
+            if(board[i][col] == 'Q') return false;
         }
 
-        // top left corner
         int i = row - 1;
         int j = col - 1;
-        while (i >= 0 && j >= 0) {
-            if (board[i][j] == 'Q')
-                return false;
+        while(i >= 0 && j >= 0){
+            if(board[i][j] == 'Q') return false;
             i--;
             j--;
         }
-
         i = row - 1;
         j = col + 1;
 
-        while (i < board.size() && j >= 0) {
-            if (board[i][j] == 'Q')
-                return false;
-
+        while(i >= 0 && j >= 0){
+            if(board[i][j] == 'Q') return false;
             i--;
             j++;
         }
-
         return true;
     }
-    void f(int idx, vector<string>& board, vector<vector<string>>& ans, int n) {
-        if (idx == n) {
+    void f(int idx , int n ,vector<string> &board, vector<vector<string>>&ans){
+        if(idx == n){
             ans.push_back(board);
             return;
         }
-        for (int i = 0; i < n; i++) {
-            if (isSafe(idx, i, board)) {
+        for(int i = 0 ; i < n ; i++){
+            if(isSafe(idx , i , board)){
                 board[idx][i] = 'Q';
-                f(idx + 1, board, ans, n);
+                f(idx +1 , n , board , ans);
                 board[idx][i] = '.';
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
+        vector<vector<string>>ans;
         vector<string> board(n , string(n, '.'));
-        f(0, board, ans, n);
+
+        f(0 , n , board , ans);
         return ans;
     }
 };
