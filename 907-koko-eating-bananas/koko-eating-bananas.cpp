66f055674214290e705e3609b2c35eb3a0f3f2eb
Naman Tyagi <namantyagi98665@gmail.com>
2026-07-18 21:53:24 +0530

    Time: 3 ms (95.09%) | Memory: 23 MB (15.88%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    long long calc(vector<int> &piles , int k){
-        long long totalh = 0;
+    long long f(vector<int>&piles , int k){
+        long long sum = 0;
         for(int i = 0 ; i < piles.size() ; i++){
-            totalh += (piles[i] + k - 1) / k;
+            sum += (piles[i] + k - 1)/k;
         }
-        return totalh;
+        return sum;
     }
-    int minEatingSpeed(vector<int>& piles, int h) {
-        long long high;
-        long long maxi = piles[0];
-        for(int i = 1 ; i < piles.size() ; i++){
-              maxi = max(maxi , 1LL *piles[i]);
-        }
-        long long ans;
-        high = maxi;
-        long long low = 1;
+    int minEatingSpeed(vector<int>& piles, int k) {
+        int n = piles.size();
+        long long maxi = LLONG_MIN;
+        long long mini = 1;
+        long long ans = -1;
+        for(int i = 0 ; i < n ; i++){
+            maxi = max(maxi , (long long)piles[i]);
+            // mini = min(mini , (long long)piles[i]);
+        } 
+        long long low = mini;
+        long long high = maxi;
         while(low <= high){
-            long long mid = low +(high-low)/2;
-
-            long long curr = calc(piles , mid);
-            if(curr <= h){
-               ans = mid;
-               high = mid -1;
+            long long mid = (low + high)/2;
+            long long curr = f(piles , mid);
+            
+            if(curr <= k){
+              ans = mid;
+              high = mid - 1;
             }
-            else{
-                low = mid + 1;
+            else if(curr > k){
+               low = mid + 1;
             }
         }
         return ans;
