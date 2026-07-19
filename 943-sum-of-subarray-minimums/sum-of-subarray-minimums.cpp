100b259057b3b97b1a465000cd6f99dd2db69f41
Naman Tyagi <namantyagi98665@gmail.com>
2026-07-19 18:34:47 +0530

    Time: 93 ms (10.98%) | Memory: 193.6 MB (26.58%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,42 +1,43 @@
 class Solution {
 public:
-    void pse(vector<int> &arr , vector<int> & prev){
+    void pse(vector<int> &prev , vector<int>&arr){
         stack<int>st;
         for(int i = 0 ; i < arr.size() ; i++){
             while(!st.empty() && arr[st.top()] > arr[i]){
                 st.pop();
             }
-            prev[i] = !st.empty() ? st.top() : -1;
+            if(!st.empty()){
+                prev[i] = st.top();
+            }
             st.push(i);
         }
     }
-
-    void nse(vector<int> &arr , vector<int> & next){
+    void nse(vector<int> &next , vector<int>&arr){
         stack<int>st;
         int n = arr.size();
-        for(int i = n -1 ; i>= 0 ; i--){
+        for(int i = n-1 ; i >= 0 ; i--){
             while(!st.empty() && arr[st.top()] >= arr[i]){
                 st.pop();
             }
-            next[i] = !st.empty() ? st.top() : n; 
+            if(!st.empty()){
+                next[i] = st.top();
+            }
             st.push(i);
         }
     }
     int sumSubarrayMins(vector<int>& arr) {
-        int MOD = 1e9 + 7;
         int n = arr.size();
         vector<int>prev(n , -1);
         vector<int>next(n , n);
-        pse(arr , prev);
-        nse(arr , next);
+        pse(prev , arr);
+        nse(next , arr);
         long long sum = 0;
-
+        int MOD = pow(10 , 9) + 7;
         for(int i = 0 ; i < n ; i++){
-            int left = i - prev[i];
-            int right = next[i] - i;
-
-            sum += (1LL * left * right * arr[i]) % MOD;
+            int x = i - prev[i];
+            int y = next[i] - i;
+            sum = (sum + 1LL * x * y * arr[i]) % MOD;
         }
-        return sum % MOD;
+        return sum;
     }
 };
