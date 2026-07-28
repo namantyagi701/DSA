7baaf5a25bdf04bf8861392df020b193056a7d5d
Naman Tyagi <namantyagi98665@gmail.com>
2026-07-28 21:09:01 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (5.41%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,22 +3,30 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<pair<int , int>> vect;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(matrix[i][j] == 0){
-                    vect.push_back({i , j});
+        vector<pair<int, int>> vect;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    vect.push_back({i, j});
                 }
             }
         }
-        for(auto it: vect){
+        unordered_set<int> rowst;
+        unordered_set<int> colst;
+        for (auto it : vect) {
             int row = it.first;
             int col = it.second;
-            for(int i = 0 ; i < m ; i++){
-                matrix[row][i] = 0;
+            if (rowst.find(row) == rowst.end()) {
+                for (int i = 0; i < m; i++) {
+                    matrix[row][i] = 0;
+                }
+                rowst.insert(row);
             }
-            for(int i = 0 ; i < n ; i++){
-                matrix[i][col] = 0;
+            if (colst.find(col) == colst.end()) {
+                for (int i = 0; i < n; i++) {
+                    matrix[i][col] = 0;
+                }
+                colst.insert(col);
             }
         }
     }
