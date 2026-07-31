3981ed56458a07905fb2bbff5f3bb4dcc9c267d8
Naman Tyagi <namantyagi98665@gmail.com>
2026-08-01 00:35:33 +0530

    Time: 19 ms (22.88%) | Memory: 31.9 MB (9.55%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& arr) {
-        vector<int>temp;
-        int n = arr.size();
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        set<int>st;
+        int n = grid.size();
+        int r = 0;
+        int m = 0;
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < n ; j++){
-                temp.push_back(arr[i][j]);
+                if(st.find(grid[i][j]) != st.end()){
+                    r = grid[i][j];
+                }
+                st.insert(grid[i][j]);
             }
         }
-        sort(temp.begin(),temp.end());
-        int m = -1;
-        int tw = -1;
-        int sum = temp[0];
-        for(int i = 1 ; i < temp.size() ; i++){
-            sum += temp[i];
-
-            if(temp[i] == temp[i-1]){
-                tw = temp[i];
-            }
+        int i = 1;
+        while(!st.empty()){
+           int front = *(st.begin());
+           st.erase(st.begin());
+           if(i != front){
+            m = i;
+            break;
+           }
+           i++;
         }
-        int x = temp.size();
-        int org = (x*(x+1))/2;
-        int diff = org - sum;
-        m = tw + diff;
-        return {tw , m};
+        if(m == 0) m = i;
+        return {r , m};
     }
 };
