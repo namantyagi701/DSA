9c64b43481b7351d887bc63fc6154bca45f3fcc1
Naman Tyagi <namantyagi98665@gmail.com>
2026-08-06 02:02:45 +0530

    Time: 0 ms (100.00%) | Memory: 77.1 MB (46.68%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,44 +11,47 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* temp = new ListNode(0);
-        ListNode* point = temp;
+        ListNode* temp1 = l1;
+        ListNode* temp2 = l2;
+        ListNode* result = new ListNode(0);
+        ListNode* head = result;
         int carry = 0;
-        while(l1 != NULL && l2 != NULL){
-            int sum = (l1-> val) + (l2 -> val) + carry;
+
+        while(temp1 != nullptr && temp2 != nullptr){
+            int d1 = temp1 -> val;
+            int d2 = temp2 -> val;
+            int sum = d1 + d2 + carry;\
             if(sum > 9) carry = 1;
             else carry = 0;
-            if(sum > 9) sum = sum % 10;
-            ListNode* node = new ListNode(sum);
-            temp -> next = node;
-            temp = node;
-            l1 = l1 -> next;
-            l2 = l2 -> next;
+            sum = sum % 10;
+            result -> next = new ListNode(sum);
+            result = result -> next;
+            temp1 = temp1 -> next;
+            temp2 = temp2 -> next;
         }
-        while(l1 != NULL){
-            int sum = carry + (l1 -> val);
+        while(temp1 != nullptr){
+            int d1 = temp1 -> val;
+            int sum = d1 + carry;
             if(sum > 9) carry = 1;
             else carry = 0;
-            if(sum > 9) sum = sum % 10;
-            ListNode* node = new ListNode(sum);
-            temp -> next = node;
-            temp = node;
-            l1 = l1 -> next;
+            sum = sum % 10;
+            result -> next = new ListNode(sum);
+            result = result -> next;
+            temp1 = temp1 -> next;
         }
-        while(l2 != NULL){
-            int sum = carry + (l2-> val);
+        while(temp2 != nullptr){
+            int d1 = temp2 -> val;
+            int sum = d1 + carry;
             if(sum > 9) carry = 1;
             else carry = 0;
-            if(sum > 9) sum = sum % 10;
-            ListNode* node = new ListNode(sum);
-            temp -> next = node;
-            temp = node;
-            l2 = l2 -> next; 
+            sum = sum % 10;
+            result -> next = new ListNode(sum);
+            result = result -> next;
+            temp2 = temp2 -> next;
         }
-        if(carry){
-            ListNode* node = new ListNode(carry);
-            temp -> next = node;
+        if(carry == 1){
+            result -> next = new ListNode(1);
         }
-        return point -> next;
+        return head -> next;
     }
 };
