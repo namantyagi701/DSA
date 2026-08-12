cbbc3678ed1e08d2ee3b1aac40eefebae6561848
Naman Tyagi <namantyagi98665@gmail.com>
2026-08-12 17:19:38 +0530

    Time: 3 ms (52.20%) | Memory: 17.9 MB (23.83%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    void f(int i , int k , vector<vector<int>>&ans, vector<int>&ds , vector<int>&arr){
-        if(i >= arr.size()){
-            if(k == 0){
-                ans.push_back(ds);
-            }
-                return;
+    void f(int i , int k , vector<int> &arr , vector<vector<int>>&ans , vector<int> &temp){
+        if(k == 0){
+           ans.push_back(temp);
+           return;
+        }
+        if(i < 0) {
+            return;
         }
 
+        f(i - 1 , k , arr ,ans , temp);
         if(arr[i] <= k){
-            ds.push_back(arr[i]);
-            f(i , k-arr[i] , ans , ds , arr);
-            ds.pop_back();
+            temp.push_back(arr[i]);
+            f(i , k - arr[i] , arr , ans , temp);
+            temp.pop_back();
         }
-        f(i + 1  , k , ans , ds ,arr);
-  
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>>ans;
-        vector<int>ds;
-        f(0 , target , ans , ds , candidates);
+        vector<int>temp;
+        int n = candidates.size();
+        f(n-1 , target , candidates , ans , temp);
         return ans;
     }
 };
