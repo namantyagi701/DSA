6023168de21959ad9d9b49e9b4cc9bf714100e48
Naman Tyagi <namantyagi98665@gmail.com>
2026-08-24 09:52:23 +0530

    Time: 383 ms (39.28%) | Memory: 11.7 MB (29.16%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,57 +1,40 @@
 class Solution {
 public:
-    bool f(int row, int col, int idx, string word, vector<vector<char>>& board,
-           vector<vector<int>>& vis) {
+    vector<int> dx = {-1, 1, 0, 0};
+    vector<int> dy = {0, 0, -1, 1};
+    bool dfs(int row, int col, int idx, vector<vector<char>>& board,
+             string word, int n, int m) {
+        if (row > n || col > m)
+            return false;
         if (idx == word.size())
             return true;
-        if (row < 0 || col < 0 || row >= board.size() || col >= board[0].size())
-            return false;
-        if (word[idx] != board[row][col])
-            return false;
 
-        int up = false;
-        int down = false;
-        int right = false;
-        int left = false;
-
-        if (idx == word.size() - 1)
-            return true;
-        vis[row][col] = 1;
-
-        if (row - 1 >= 0 && vis[row - 1][col] != 1) {
-
-            up = f(row - 1, col, idx + 1, word, board, vis);
+        for (int j = 0; j < 4; j++) {
+            int nRow = row + dx[j];
+            int nCol = col + dy[j];
+            if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < m &&
+                board[nRow][nCol] == word[idx] && board[nRow][nCol] != '.') {
+                char org = board[nRow][nCol];
+                board[nRow][nCol] = '.';
+                if (dfs(nRow, nCol, idx + 1, board, word, n, m))
+                    return true;
+                board[nRow][nCol] = org;
+            }
         }
-
-        if (row + 1 < board.size() && vis[row + 1][col] != 1) {
-
-            down = f(row + 1, col, idx + 1, word, board, vis);
-        }
-        if (col - 1 >= 0 && vis[row][col - 1] != 1) {
-
-            left = f(row, col - 1, idx + 1, word, board, vis);
-        }
-        if (col + 1 < board[0].size() && vis[row][col + 1] != 1) {
-
-            right = f(row, col + 1, idx + 1, word, board, vis);
-        }
-        vis[row][col] = 0;
-
-        return up || down || left || right;
+        return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
         int n = board.size();
         int m = board[0].size();
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (board[i][j] == word[0]) {
-                    if (word.size() == 1)
+                    char org = board[i][j];
+                    board[i][j] = '.';
+                    bool ans = dfs(i, j, 1, board, word, n, m);
+                    if (ans)
                         return true;
-                    vector<vector<int>> vis(n, vector<int>(m, 0));
-                    bool res = f(i, j, 0, word, board, vis);
-                    if (res)
-                        return true;
+                    board[i][j] = org;
                 }
             }
         }
