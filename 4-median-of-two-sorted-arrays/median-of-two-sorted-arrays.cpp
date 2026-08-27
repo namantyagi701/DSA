35f45b03de904b893718b5bb621b314f1ca2ca07
Naman Tyagi <namantyagi98665@gmail.com>
2026-08-28 00:16:36 +0530

    Time: 0 ms (100.00%) | Memory: 95 MB (94.79%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -3,34 +3,36 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size();
         int n2 = nums2.size();
-        if (n2 < n1)
-            return findMedianSortedArrays(nums2, nums1);
-        int totalL = (n1 + n2 + 1) / 2;
+        if(n2 < n1) return findMedianSortedArrays(nums2 , nums1);
+
+        int totalL = (n1 + n2 + 1)/2;
         int low = 0;
         int high = n1;
-        while (low <= high) {
-            int mid1 = (low + high) / 2;
-            int mid2 = totalL - mid1;
+        while(low <= high){
+            int cut1 = (low + high)/2;
+            
+            int cut2 = totalL - cut1;
 
-            int l1 = mid1 > 0 ? nums1[mid1 - 1] : INT_MIN;
-            int l2 = mid2 > 0 ? nums2[mid2 - 1] : INT_MIN;
-            int r1 = mid1 < n1 ? nums1[mid1] : INT_MAX;
-            int r2 = mid2 < n2 ? nums2[mid2] : INT_MAX;
+            int l1 = cut1 == 0 ? INT_MIN : nums1[cut1-1];
+            int l2 = cut2 == 0 ? INT_MIN : nums2[cut2-1];
+            int r1 = cut1 == n1 ? INT_MAX : nums1[cut1];
+            int r2 = cut2 == n2 ? INT_MAX : nums2[cut2];
 
-            if (l1 <= r2 && l2 <= r1) {
-                if (((n1 + n2) % 2) == 1) {
-                   return max(l1, l2);
-                } else {
-                    return (max(l1 , l2) + min(r1 , r2)) /2.0;
+            if(l1 <= r2 && l2 <= r1){
+                if((n1 + n2) % 2 == 0){
+                    return (max(l1 , l2) + min(r1 , r2))/2.0;
+                }
+                else{
+                    return max(l1 , l2);
                 }
             }
             else if(l1 > r2){
-                high = mid1-1;
+                   high = cut1 -1;
             }
             else{
-                low = mid1 + 1;
+                low = cut1 + 1;
             }
         }
-        return 0;
+        return -1;
     }
 };
