f291acf3c4ecca21235c1a92e0bf577c944cfbc6
Naman Tyagi <namantyagi98665@gmail.com>
2026-09-15 01:06:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (46.89%) - LeetSync

---
+++ 866-rectangle-overlap/rectangle-overlap.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rect1, vector<int>& rect2) {
+        return rect1[0] < rect2[2] && rect2[0] < rect1[2] && rect1[1] < rect2[3]
+        && rect2[1] < rect1[3];
+    }
+};
